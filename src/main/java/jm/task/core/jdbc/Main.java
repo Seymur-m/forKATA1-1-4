71db124a3f158016ff27c5be1bf2051e2cb02271
Сеймур Мамедов <146606145+Seymur-m@users.noreply.github.com>
2024-09-01 20:03:27 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        Util.getConnection();
+        UserServiceImpl user = new UserServiceImpl();
+
+        user.createUsersTable();
+        user.saveUser("Igor", "Shubin", (byte) 27);
+        user.saveUser("Alexander", "Pulemetov", (byte) 80);
+        user.saveUser("Jack", "Golubev", (byte) 50);
+        user.saveUser("Marsel", "Evdakimov", (byte) 6);
+
+        user.removeUserById(2);
+        user.getAllUsers();
+        user.cleanUsersTable();
+        user.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private final static String URL = "jdbc:mysql://localhost:3306/testforkata";
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "root";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("Connection is done!");
+            }
+        } catch (SQLException e) {
+            System.out.println("Connection error!");
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
